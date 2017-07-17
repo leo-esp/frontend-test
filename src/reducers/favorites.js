38492530600661bc265f2bc83a0d2bd5073229bc
Leonardo Esposito <leo_esp_5@hotmail.com>
2017-07-17 01:52:39 -0300

    retirando utilização do método includes do ES7

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -2,7 +2,7 @@ import {List} from 'immutable';
 
 export function favorite(state = new List(), action) {
     if (action.type === 'FAVORITE') {
-        if(state.includes(action.movie)){
+        if(state.filter(movie => movie.unit === action.movie.unit).length > 0){
             return new List(state.filter(movie => movie.unit !== action.movie.unit))
         }else{
             return new List(state.concat(action.movie));
